7092c2d0733c9cf8d555ca854428d4584d4cadf2
cmulliss <cmulliss@gmail.com>
2021-07-07 16:57:53 +0100

    search

--- monst-rolodex/src/components/App.js
+++ monst-rolodex/src/components/App.js
@@ -5,7 +5,8 @@ import CardList from './cardList/CardList'
 // initial state is an empty array
 class App extends Component {
 	state = {
-		monsters: []
+		monsters: [],
+		searchField: ''
 	}
 
 	async componentDidMount() {
@@ -13,15 +14,23 @@ class App extends Component {
 		const json = await response.json()
 		this.setState({ monsters: json })
 	}
+	// going to pass in monsters as a prop from our App, to CardList
 
 	render() {
+		// filter our monsters by creating a new [] in render, thus leaving orginal [] intact, start with destructuring
+		const { monsters, searchField } = this.state
+		const filteredMonsters = monsters.filter((monster) => monster.name.toLowerCase().includes(searchField.toLowerCase()))
 		return (
-			<div>
-				<CardList>
-					{this.state.monsters.map((monster, index) => (
-						<h1 key={index}>{monster.name}</h1>
-					))}
-				</CardList>
+			<div className='App'>
+				<input
+					type='search'
+					placeholder='search monsters'
+					onChange={(e) => {
+						this.setState({ searchField: e.target.value })
+					}}
+				/>
+
+				<CardList monsters={this.state.monsters}></CardList>
 			</div>
 		)
 	}
@@ -31,3 +40,17 @@ export default App
 
 // we are going to wait for our component to mount, then fetch all our users, than update our state 'monsters' with the new [] of users.
 // taking the response and converting it into the json format that js can understand and use.
+/*
+<input
+					type='search'
+					placeholder='search monsters'
+					onChange={(e) => {
+						this.setState(
+							{ searchField: e.target.value },
+							//callback, runs after async setState has completed, ie updated state
+							() => console.log(this.state)
+						)
+					}}
+				/>
+
+*/
