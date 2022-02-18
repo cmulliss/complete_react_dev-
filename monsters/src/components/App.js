d3476f1bf9c3da0dc9ab1985cc502226bd5a9303
cmulliss <cmulliss@gmail.com>
2022-02-18 16:52:23 +0000

    components

--- monsters/src/components/App.js
+++ monsters/src/components/App.js
@@ -1,11 +1,36 @@
-import './App.css'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      <div>App</div>
-    </div>
-  )
+import CardList from './cardlist/CardList'
+
+class App extends Component {
+  state = {
+    monsters: []
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <CardList name='Cherry' />
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App
+
+// There is my solution using async await :
+
+//   async componentDidMount() {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const json = await response.json();
+//     this.setState({ users: json });
+//   }
